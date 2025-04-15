70153723b1de2cf556e41d2243f4fe3c2c492a86
Renaudin clement <cronisosdev@gmail.com>
2025-04-15 16:35:09 +0200

    mise en place d'un map pour changer les reponse en fonction d'un questionnaire

--- js/Views/pages/Question.js
+++ js/Views/pages/Question.js
@@ -5,9 +5,40 @@ export default class Question extends Page_comp {
       
         return Page_comp.renderPage(() => `
       <section class="main-content">
-        <p> test </p>
+        <p> ${reponseQuestionParId.get("1")} </p>
 
       </section>
     `);
     }
 }
+
+//dictionnaire des question
+
+let reponseQuestionParId = new Map([
+    ["1", "coq corico"],
+    ["2", "i don't know why i am here"],
+    ["3", "trop cool"],
+    ["4", "tu peux le faire"],
+    ["5", "serpincroyable"],
+    ["6", "sois pas volatile"],
+    ["7", "rapide comme un wallabies"],
+    ["8", "tu ravi roux !"],
+    ["9", "tapis tapis tapis"],
+    ["10", "oeil de lynx"],
+    ["11", "presque a la fin"]
+  ]);
+
+
+// changer reponse reponseQuestionParId.set("1", "woof");
+
+
+
+console.log(reponseQuestionParId.size);
+console.log(reponseQuestionParId.get("2"));
+console.log(reponseQuestionParId.has("2"));
+reponseQuestionParId.delete("1");
+
+for (let [key, value] of reponseQuestionParId) {
+  console.log(key + " dis a la personne " + value);
+}
+
