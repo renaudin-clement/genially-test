ffa9ec60e3663fa42005b8f40b137a165332609b
Renaudin clement <cronisosdev@gmail.com>
2025-04-16 16:58:20 +0200

    petit modif code

--- js/Views/pages/resultat.js
+++ js/Views/pages/resultat.js
@@ -17,17 +17,14 @@ export default class Resultat extends Page_comp {
         console.log("Nombre total de questions :", total);
         return Page_comp.renderPage(() => `
 
-<section>    
-    <div class="score-box">
-        Oeuf total : <span id="scoreDisplay">${total}/10</span>
-    </div>
-
-    <div class="score-box">
-        point total : <span id="scoreDisplay">-/10</span>
-    </div>
-
- 
-    </section>
+                <section>    
+                    <div class="score-box">
+                        Oeuf total : <span id="scoreDisplay">${total}/10</span>
+                    </div>
+                    <div class="score-box">
+                        point total : <span id="scoreDisplay">-/10</span>
+                    </div>
+                </section>
 
     `);
     }
